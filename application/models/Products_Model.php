9579a8173accbac6f4e2e3e2c08ea1746b686d92
Paintcast <paintcast@gmail.com>
2016-01-06 21:23:57 +0200

    Revert "Добавлено отображение страницы с информацией о товаре"
    This reverts commit 93edeec158cb44b0cc8d5f2d84ff94d9eb989d19.

--- application/models/Products_Model.php
+++ application/models/Products_Model.php
@@ -24,22 +24,4 @@ class Products_Model extends LOFT_Model
         return $result->result_array();
     }
 
-//    TODO: в getProductByID дописать джоин id_category, id_brand
-
-    /**
-     *
-     * Метод получения информации из БД по товару с ID = $id
-     *
-     * @param $id - ID товара для поиска
-     * @return mixed - инфа о товаре
-     */
-    public function getProductById($id)
-    {
-        $this->db->where(array('id'=>$id));
-        $result = $this->db->get($this->table);
-        return $result->row();
-    }
-
-
-
 }
